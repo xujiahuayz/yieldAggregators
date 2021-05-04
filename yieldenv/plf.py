59a4b11cfafea4be53b30e32ff7011ccb99acfb3
Simon Cousaert <43789598+SimonCousaert@users.noreply.github.com>
2021-05-04 14:20:39 +0200

    add User to plf file

--- yieldenv/plf.py
+++ yieldenv/plf.py
@@ -2,51 +2,58 @@ from dataclasses import dataclass
 import dataclasses
 import logging
 import numpy as np
-from numpy.core.arrayprint import ComplexFloatingFormat
 
-
-@dataclass
 class User:
-    name: str
+    def __init__(self, name: str):
+        self.user_collected_tokens = 0
+        self.user_deposit_available_as_collateral = 0
+        self.user_interest_revenue = 0
+        self.user_paid_interest = 0
+        self.name = name
 
-    def __post__init__(self):
-        self.collected_tokens = 0
-        self.deposit_available_as_collateral = 0
+    def getProfit(self, token_price):
+
+        fees = self.user_interest_revenue - self.user_paid_interest
+        tokens = self.user_collected_tokens
+        value = tokens * token_price
+
+        return (
+            "Collected fees (in supplied token): "
+            + str(fees)
+            + ". Collected tokens: "
+            + str(tokens)
+            + ", worth: "
+            + str(value)
+        )
 
 
 class Plf:
     def __init__(
         self,
-        supply_rate: float,
-        borrow_rate: float,
+        supply_apr: float,
+        borrow_apr: float,
         distribution_per_day: float,
         total_available_funds: float = 100,
         collateral_ratio: float = 1.2,
     ):
-        self.supply_apr = supply_rate
-        self.borrow_apr = borrow_rate
+        self.supply_apr = supply_apr
+        self.borrow_apr = borrow_apr
         self.total_available_funds = total_available_funds
         self.collateral_ratio = (
             collateral_ratio
             # collateral divided by amount able to borrow
         )
         self.distribution_per_day = distribution_per_day
-        self.user_interest_revenue = 0
-        self.paid_fees = 0
-        self.user_collected_tokens = 0
-        self.user_deposit_available_as_collateral = (
-            0  # imagine there is only one user interacting with the pool
-        )
-        # self.user_borrow = 0
+        
 
     def supply(self, amount, days, user: User):
 
         self.total_available_funds += amount
-        self.user_interest_revenue += amount * (self.supply_apr / 365) * days
+        user.user_interest_revenue += amount * (self.supply_apr / 365) * days
 
-        self.user_deposit_available_as_collateral += amount
+        user.user_deposit_available_as_collateral += amount
 
-        self.user_collected_tokens += (
+        user.user_collected_tokens += (
             amount / self.total_available_funds * self.distribution_per_day * days
         )  # this assumes that the portion of supplied tokens is constant throughout the supplying period
 
@@ -59,10 +66,10 @@ class Plf:
         # self.user_deposit_available_as_collateral -= amount
         self.total_available_funds -= amount
 
-        self.paid_fees += amount * (self.borrow_apr / 365) * days
+        user.user_paid_interest += amount * (self.borrow_apr / 365) * days
 
-        User.deposit_available_as_collateral -= amount
-        User.collected_tokens += (
+        user.deposit_available_as_collateral -= amount
+        user.collected_tokens += (
             amount / self.total_available_funds * self.distribution_per_day * days
         )  # this assumes that the portion of supplied tokens is constant throughout the supplying period
 
@@ -70,18 +77,3 @@ class Plf:
         collateral = amount * self.collateral_ratio
         self.supply(amount=collateral, days=days)
         self.borrow(amount=amount, days=days)
-
-    def getProfit(self, token_price):
-
-        fees = self.user_interest_revenue - self.paid_fees
-        tokens = self.user_collected_tokens
-        value = tokens * token_price
-
-        return (
-            "Collected fees (in supplied token): "
-            + str(fees)
-            + ". Collected tokens: "
-            + str(tokens)
-            + ", worth: "
-            + str(value)
-        )
