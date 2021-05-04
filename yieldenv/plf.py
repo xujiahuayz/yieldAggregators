5c4b2904e3ac362762719b4e6191aeee758aa73c
Jiahua Xu <xujiahuayz@gmail.com>
2021-05-04 12:18:45 +0100

    modify plf

--- yieldenv/plf.py
+++ yieldenv/plf.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import logging
 import numpy as np
+from numpy.core.arrayprint import ComplexFloatingFormat
 
 
 class Plf:
@@ -16,45 +17,53 @@ class Plf:
         self.borrow_apr = borrow_rate
         self.total_available_funds = total_available_funds
         self.collateral_ratio = (
-            collateral_ratio  # collateral divided by amount able to borrow
+            collateral_ratio
+            # collateral divided by amount able to borrow
         )
         self.distribution_per_day = distribution_per_day
-        self.collected_fees = 0
+        self.user_interest_revenue = 0
         self.paid_fees = 0
-        self.collected_tokens = 0
+        self.user_collected_tokens = 0
+        self.user_deposit_available_as_collateral = (
+            0  # imagine there is only one user interacting with the pool
+        )
+        # self.user_borrow = 0
 
     def supply(self, amount, days):
 
-        self.total_available_funds -= amount
-        self.collected_fees += amount * (self.supply_apr / 365) * days
+        self.total_available_funds += amount
+        self.user_interest_revenue += amount * (self.supply_apr / 365) * days
 
-        self.collected_tokens += (
+        self.user_deposit_available_as_collateral += amount
+
+        self.user_collected_tokens += (
             amount / self.total_available_funds * self.distribution_per_day * days
         )  # this assumes that the portion of supplied tokens is constant throughout the supplying period
 
-    def borrow(self, amount, days):
-
+    def borrow(self, amount: float, days: float):
         collateral = amount * self.collateral_ratio
-
         assert (
-            self.total_available_funds + collateral - amount >= 0
-        ), "Not enough funds to borrow"  # can only happen in case collateral_ratio < 1
+            self.user_deposit_available_as_collateral >= collateral
+        ), "Borrow position under-collateralized"
 
-        self.total_available_funds += (
-            collateral  # to borrow x, you first have to deposit x * collateral_ratio
-        )
+        self.user_deposit_available_as_collateral -= amount
         self.total_available_funds -= amount
-        self.paid_fees += amount * (self.borrow_apr / 365) * days
-        self.collected_fees += collateral * (self.supply_apr / 365) * days
 
-        self.collected_tokens += (
-            collateral / self.total_available_funds * self.distribution_per_day * days
+        self.paid_fees += amount * (self.borrow_apr / 365) * days
+
+        self.user_collected_tokens += (
+            amount / self.total_available_funds * self.distribution_per_day * days
         )  # this assumes that the portion of supplied tokens is constant throughout the supplying period
+
+    def supply_then_borrow(self, amount, days):
+        collateral = amount * self.collateral_ratio
+        self.supply(amount=collateral, days=days)
+        self.borrow(amount=amount, days=days)
 
     def getProfit(self, token_price):
 
-        fees = self.collected_fees - self.paid_fees
-        tokens = self.collected_tokens
+        fees = self.user_interest_revenue - self.paid_fees
+        tokens = self.user_collected_tokens
         value = tokens * token_price
 
         return (
