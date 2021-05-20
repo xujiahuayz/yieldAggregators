a043037aafa76b9f950beb5245a5e7bb51e43e1a
Jiahua Xu <xujiahuayz@gmail.com>
2021-05-20 19:49:28 +0100

    update

--- playground/empirical_plots.py
+++ playground/empirical_plots.py
@@ -5,12 +5,12 @@ from yieldenv.constants import CONTRACT_DATA, DATA_PATH
 
 import matplotlib.pyplot as plt
 
-BLOCK_SERIES_NAME = "blocks"
-PPS_SERIES_NAME = "pps"
+BLOCK_NUMBER = "blocks"
+VALUE = "pps"
 
 for name, value in CONTRACT_DATA.items():
     for key, func in value["function"].items():
-        result = {BLOCK_SERIES_NAME: [], PPS_SERIES_NAME: []}
+        result = {BLOCK_NUMBER: [], VALUE: []}
         with gzip.open(
             os.path.join(
                 DATA_PATH, f"{value['protocol']}_{value['asset']}_{key}.jsonl.gz"
@@ -18,9 +18,11 @@ for name, value in CONTRACT_DATA.items():
         ) as f:
             for _, w in enumerate(f):
                 this_block = json.loads(w)
-                result[BLOCK_SERIES_NAME].append(this_block[0])
-                result[PPS_SERIES_NAME].append(this_block[1])
-            plt.plot(result[BLOCK_SERIES_NAME], result[PPS_SERIES_NAME])
+                if this_block[1] < 100:
+                    next
+                result[BLOCK_NUMBER].append(this_block[0])
+                result[VALUE].append(this_block[1] / 1e24)
+            plt.plot(result[BLOCK_NUMBER], result[VALUE])
             plt.title(f"{value['protocol']}_{value['asset']}_{key}")
             plt.show()
             plt.close()
