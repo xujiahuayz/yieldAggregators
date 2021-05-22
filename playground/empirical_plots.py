bc990ab06eca6baa3a91f12f0bb20eb83f44ba58
Simon Cousaert <simon.cousaert@outlook.com>
2021-05-22 14:01:20 +0200

    update empirical plots

--- playground/empirical_plots.py
+++ playground/empirical_plots.py
@@ -12,6 +12,17 @@ IMAGE_PATH = os.path.join(PROJECT_ROOT, "assets")
 BLOCK_NUMBER = "blocks"
 VALUE = "pps"
 
+axes_fontsize = 18
+ticks_fontsize = 18
+title_fontsize = 18
+legend_title_fontsize = 17
+legend_fontsize = 17
+magnitude_fontsize = 16
+ylim_lower = 0.82
+ylim_upper = 1.065
+
+
+
 
 # --------------- vault DAI supply ---------------------
 
@@ -31,15 +42,24 @@ for name, value in CONTRACT_DATA.items():
                         #     next
                         result[BLOCK_NUMBER].append(this_block[0])
                         result[VALUE].append(this_block[1] / (10**int(value['decimals'])))
-                    print(result[BLOCK_NUMBER][0])
                     date_range = pd.date_range(value['start_date'], value['end_date'], periods = len(result[BLOCK_NUMBER]))
                     plt.plot(date_range, result[VALUE], label = f"{value['protocol']}")
                     plt.gcf().autofmt_xdate()
                     title=f"vault{value['asset']}: {key}"
-                    plt.title(title)
-
-plt.legend(loc='upper left')
-fig_path = os.path.join(IMAGE_PATH, f"{title}.pdf")
+                    plt.title(title, fontsize=title_fontsize)
+                    
+plt.xlabel(xlabel="Date", fontsize=axes_fontsize)
+plt.xticks(fontsize=ticks_fontsize)
+plt.yticks(fontsize=ticks_fontsize)
+plt.gca().yaxis.get_offset_text().set_size(magnitude_fontsize)
+# plt.ylim(ylim_lower, ylim_upper)
+plt.legend(
+    loc='upper left',
+    fontsize=legend_fontsize,
+    title_fontsize=legend_title_fontsize,
+    labelspacing=0.1,
+    frameon=True,)
+fig_path = os.path.join(IMAGE_PATH, f"vault_DAI_total_supply.pdf")
 plt.savefig(fig_path)
 plt.show()
 plt.close()
@@ -62,15 +82,24 @@ for name, value in CONTRACT_DATA.items():
                         if this_block[1] / 1e18 > 0.1:
                             result[BLOCK_NUMBER].append(this_block[0])
                             result[VALUE].append(this_block[1] / (10**int(value['decimals'])))
-                    print(result[BLOCK_NUMBER][0])
                     date_range = pd.date_range(value['start_date'], value['end_date'], periods = len(result[BLOCK_NUMBER]))
                     plt.plot(date_range, result[VALUE], label = f"{value['protocol']}")
                     plt.gcf().autofmt_xdate()
                     title=f"vault{value['asset']}: {key}"
-                    plt.title(title)
+                    plt.title(title, fontsize=title_fontsize)
 
-plt.legend(loc='upper left')
-fig_path = os.path.join(IMAGE_PATH, f"{title}.pdf")
+plt.xlabel(xlabel="Date", fontsize=axes_fontsize)
+plt.xticks(fontsize=ticks_fontsize)
+plt.yticks(fontsize=ticks_fontsize)
+plt.gca().yaxis.get_offset_text().set_size(magnitude_fontsize)
+plt.ylim(ylim_lower, ylim_upper)
+plt.legend(
+    loc='lower left',
+    fontsize=legend_fontsize,
+    title_fontsize=legend_title_fontsize,
+    labelspacing=0.1,
+    frameon=True,)
+fig_path = os.path.join(IMAGE_PATH, f"vault_DAI_price_per_share.pdf")
 plt.savefig(fig_path)
 plt.show()
 plt.close()
@@ -93,15 +122,24 @@ for name, value in CONTRACT_DATA.items():
                         #     next
                         result[BLOCK_NUMBER].append(this_block[0])
                         result[VALUE].append(this_block[1] / (10**int(value['decimals'])))
-                    print(result[BLOCK_NUMBER][0])
                     date_range = pd.date_range(value['start_date'], value['end_date'], periods = len(result[BLOCK_NUMBER]))
                     plt.plot(date_range, result[VALUE], label = f"{value['protocol']}")
                     plt.gcf().autofmt_xdate()
                     title=f"vault{value['asset']}: {key}"
-                    plt.title(title)
+                    plt.title(title, fontsize=title_fontsize)
 
-plt.legend(loc='upper left')
-fig_path = os.path.join(IMAGE_PATH, f"{title}.pdf")
+plt.xlabel(xlabel="Date", fontsize=axes_fontsize)
+plt.xticks(fontsize=ticks_fontsize)
+plt.yticks(fontsize=ticks_fontsize)
+plt.gca().yaxis.get_offset_text().set_size(magnitude_fontsize)
+# plt.ylim(ylim_lower, ylim_upper)
+plt.legend(
+    loc='upper left',
+    fontsize=legend_fontsize,
+    title_fontsize=legend_title_fontsize,
+    labelspacing=0.1,
+    frameon=True,)
+fig_path = os.path.join(IMAGE_PATH, f"vault_USDC_total_supply.pdf")
 plt.savefig(fig_path)
 plt.show()
 plt.close()
@@ -125,15 +163,24 @@ for name, value in CONTRACT_DATA.items():
                         if this_block[1] / 1e6 > 0.1:
                             result[BLOCK_NUMBER].append(this_block[0])
                             result[VALUE].append(this_block[1] / 1e6)
-                    print(result[BLOCK_NUMBER][0])
                     date_range = pd.date_range(value['start_date'], value['end_date'], periods = len(result[BLOCK_NUMBER]))
                     plt.plot(date_range, result[VALUE], label = f"{value['protocol']}")
                     plt.gcf().autofmt_xdate()
                     title=f"vault{value['asset']}: {key}"
-                    plt.title(title)
+                    plt.title(title, fontsize=title_fontsize)
 
-plt.legend(loc='upper left')
-fig_path = os.path.join(IMAGE_PATH, f"{title}.pdf")
+plt.xlabel(xlabel="Date", fontsize=axes_fontsize)
+plt.xticks(fontsize=ticks_fontsize)
+plt.yticks(fontsize=ticks_fontsize)
+plt.gca().yaxis.get_offset_text().set_size(magnitude_fontsize)
+plt.ylim(ylim_lower, ylim_upper)
+plt.legend(
+    loc='center right',
+    fontsize=legend_fontsize,
+    title_fontsize=legend_title_fontsize,
+    labelspacing=0.1,
+    frameon=True,)
+fig_path = os.path.join(IMAGE_PATH, f"vault_USDC_price_per_share.pdf")
 plt.savefig(fig_path)
 plt.show()
 plt.close()
@@ -157,15 +204,24 @@ for name, value in CONTRACT_DATA.items():
                         #     next
                         result[BLOCK_NUMBER].append(this_block[0])
                         result[VALUE].append(this_block[1] / (10**int(value['decimals'])))
-                    print(result[BLOCK_NUMBER][0])
                     date_range = pd.date_range(value['start_date'], value['end_date'], periods = len(result[BLOCK_NUMBER]))
                     plt.plot(date_range, result[VALUE], label = f"{value['protocol']}")
                     plt.gcf().autofmt_xdate()
                     title=f"vault{value['asset']}: {key}"
-                    plt.title(title)
+                    plt.title(title, fontsize=title_fontsize)
 
-plt.legend(loc='upper left')
-fig_path = os.path.join(IMAGE_PATH, f"{title}.pdf")
+plt.xlabel(xlabel="Date", fontsize=axes_fontsize)
+plt.xticks(fontsize=ticks_fontsize)
+plt.yticks(fontsize=ticks_fontsize)
+plt.gca().yaxis.get_offset_text().set_size(magnitude_fontsize)
+# plt.ylim(ylim_lower, ylim_upper)
+plt.legend(
+    loc='upper right',
+    fontsize=legend_fontsize,
+    title_fontsize=legend_title_fontsize,
+    labelspacing=0.1,
+    frameon=True,)
+fig_path = os.path.join(IMAGE_PATH, f"vault_3crv_total_supply.pdf")
 plt.savefig(fig_path)
 plt.show()
 plt.close()
@@ -187,46 +243,24 @@ for name, value in CONTRACT_DATA.items():
                         if this_block[1] / 1e18 > 0.1:
                             result[BLOCK_NUMBER].append(this_block[0])
                             result[VALUE].append(this_block[1] / 1e18)
-                    print(result[BLOCK_NUMBER][0])
                     date_range = pd.date_range(value['start_date'], value['end_date'], periods = len(result[BLOCK_NUMBER]))
                     plt.plot(date_range, result[VALUE], label = f"{value['protocol']}")
                     plt.gcf().autofmt_xdate()
                     title=f"vault{value['asset']}: {key}"
-                    plt.title(title)
+                    plt.title(title, fontsize=title_fontsize)
 
-plt.legend(loc='upper left')
-fig_path = os.path.join(IMAGE_PATH, f"{title}.pdf")
+plt.xlabel(xlabel="Date", fontsize=axes_fontsize)
+plt.xticks(fontsize=ticks_fontsize)
+plt.yticks(fontsize=ticks_fontsize)
+plt.gca().yaxis.get_offset_text().set_size(magnitude_fontsize)
+plt.ylim(ylim_lower, ylim_upper)
+plt.legend(
+    loc='lower left',
+    fontsize=legend_fontsize,
+    title_fontsize=legend_title_fontsize,
+    labelspacing=0.1,
+    frameon=True,)
+fig_path = os.path.join(IMAGE_PATH, f"vault_3crv_price_per_share.pdf")
 plt.savefig(fig_path)
 plt.show()
 plt.close()
-
-
-
-
-
-# --------------------------------------------------------------
-
-# result = {BLOCK_NUMBER: [], VALUE: []}
-# for _, w in enumerate(harvest_dai_data):
-#     this_block = json.loads(w)
-#     result[BLOCK_NUMBER].append(this_block[0])
-#     result[VALUE].append(this_block[1] / 1e24)
-
-# plt.plot(result[BLOCK_SERIES_NAME], result[PPS_SERIES_NAME])
-
-#         get_onchain_data(
-#             abi_address=value["abi_address"],
-#             contract_address=value["contract_address"],
-#             start_no=value["start_block"],
-#             end_no=value["end_block"],
-#             file_name=f"{value['protocol']}_{value['asset']}_{key}",
-#             function_name=func,
-#         )
-
-# for name, value in CONTRACT_DATA.items():
-#     result = {BLOCK_SERIES_NAME: [], PPS_SERIES_NAME: []}
-#     with gzip.open(os.path.join(DATA_PATH, f"{name}_pricePerShare.jsonl.gz")) as f:
-#         for _, w in enumerate(f):
-#             this_block = json.loads(w)
-#             result[BLOCK_SERIES_NAME].append(this_block[0])
-#             result[PPS_SERIES_NAME].append(this_block[1])
